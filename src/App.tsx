84cf8ca500220de7b7ff3b94b955a214fc6c5558
Gabriela Grossmann <gahgrossmann@gmail.com>
2024-10-23 20:10:53 +0200

    PokemonCard dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import PokemonCard from './components/PokemonCard';
 
 const App = () => {
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-
-const PokemonCard = () => {
-  return (
-    <figure>
-      <img 
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
-  );
-};
-
-export default PokemonCard;
+// Declarar pokemonList primeiro
+const pokemonList = [
+	{
+	  name: "bulbasaur",
+	  imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+	  name: "mew",
+	},
+  ];
+  function PokemonCard() {
+	const pokemon = pokemonList[1]; // Segundo Pokémon (Mew)
+	
+	return (
+	  <figure>
+		{pokemon.imgSrc ? (
+		  <img src={pokemon.imgSrc} alt={pokemon.name} />
+		) : (
+		  <p>???</p>
+		)}
+		<figcaption>{pokemon.name}</figcaption>
+	  </figure>
+	);
+  }
+  
+  export default PokemonCard;
